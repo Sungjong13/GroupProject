71c04e1d0b12838ce4547d115b8a10b93261f0f2
Sungjong Oh <sungjongoh1@gmail.com>
2022-10-21 15:27:07 +0900

    POST method map update function added

--- project_name/map/views.py
+++ project_name/map/views.py
@@ -3,25 +3,56 @@ from pathlib import Path
 import os
 import json
 from django.core.exceptions import ImproperlyConfigured
+import requests
 
 
-def get_secret(setting,secrets):
+def get_key(setting):
+    '''
+    returns kakao api key in secrets.json
+    '''
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    secret_file = os.path.join(BASE_DIR, 'secrets.json')
+    with open(secret_file) as f:
+        secrets = json.loads(f.read())
     try:
         return secrets[setting]
     except KeyError:
         error_msg = "set the {0} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
 
+def get_latlon(address):
+    '''
+    input string address, return latitude longitude as tuple.
+    '''
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + address
+    api_key = get_key("JS_KEY")
+    headers = {"Authorization": f"KakaoAK {api_key}"}
+    api_json = json.loads(str(requests.get(url,headers=headers).text))
+    if api_json['documents']:
+        address = api_json['documents'][0]
+    else:
+        # error 값 2
+        return(2,2)
+
+    return (address['y'], address['x'])
+
 # Create your views here.
 def map_window(request):
     # get kakao api key
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    secret_file = os.path.join(BASE_DIR, 'secrets.json')
-    with open(secret_file) as f:
-        secrets = json.loads(f.read())
-    KAKAO_API_KEY = get_secret("KAKAO_API_KEY",secrets)
+    KAKAO_API_KEY = get_key("KAKAO_API_KEY")
 
-    return render(request, 'map/map_window.html', {"KAKAO_API_KEY":KAKAO_API_KEY})
+    # 사용자 입력 주소 처리
+    if request.method == "POST":
+        address = request.POST["address"]
+        lat,lon = get_latlon(address)
+    else:
+        # default 값 1
+        lat,lon = (1, 1)
 
+    return render(request, 'map/map_window.html', {"KAKAO_API_KEY":KAKAO_API_KEY, "lat":lat, "lon":lon})
+
+if __name__ == '__main__':
+    pass
+    get_latlon('대천로 103번길')
 
 
--- project_name/static/js/map.js
+++ project_name/static/js/map.js
@@ -36,3 +36,23 @@
 //       });
 //     return position;
 //   }();
+
+//geocoder backup
+// 주소-좌표 변환 객체를 생성합니다
+//var geocoder = new kakao.maps.services.Geocoder();
+// 주소로 좌표를 검색합니다
+//geocoder.addressSearch('{{address}}', function(result, status) {
+// 정상적으로 검색이 완료됐으면
+//if (status === kakao.maps.services.Status.OK) {
+// 맵 생성
+//showmap(33.450701,126.570667);
+//var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+//map.setCenter(coords);
+//} else {
+// 주소 인식 안되면 사용자위치로 map 생성
+//alert("카카오 서비스가 주소를 인식하지 못했습니다.");
+//currentposmap();
+//}
+//});
+// 기입 받은 address 없을 때
